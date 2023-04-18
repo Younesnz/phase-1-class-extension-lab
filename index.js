347fc015b5ed60a5dd16c4cf2a2d1e8d7f17fab1
Younes Nourzehi <unesnz@gmail.com>
2023-04-18 15:23:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    return this.sides.reduce((acc, value) => acc + value, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    return this.sides.every((value) => value < this.perimeter - value);
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sides.every((value) => value === this.sides[0]);
+  }
+  get area() {
+    return this.sides[0] ** 2;
+  }
+}
